6c22e4609def1bfdb34a52162698cd8cb3a2cca8
PMengler <114950259+PMengler@users.noreply.github.com>
2023-03-09 11:42:19 -0800

    set up file structureand wrote connection and model files

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+// const thoughtSchema = require('./thought');
+// const reactionSchema = require('./reaction');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'], // check this is valid mongoose validation for email
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
